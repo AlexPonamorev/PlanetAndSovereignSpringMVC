ffb295a8f5cf21fbd6d4d81aa6baf1baf5f38f21
AlexPonamorev <ponamorev.alex2012@gmail.com>
2021-06-14 18:31:51 +0300

    улучшен метод рандомизации

--- src/main/java/com/example/TaskSpringBoot1/services/FactoryObjects.java
+++ src/main/java/com/example/TaskSpringBoot1/services/FactoryObjects.java
@@ -13,6 +13,7 @@ public class FactoryObjects {
         Set<Planet> planetSet = new HashSet<>();
         List<String> planetNames = Arrays.asList("Ahch-To", "Anoat", "Bespin", "Cantonica", "Christophsis",
                 "Coruscant", "Dagobah", "Eadu", "Endor", "Exegol", "Felucia", "Genesis");
+        Collections.shuffle(planetNames, new Random(3));
         Set<String> nameSet = new HashSet<>(planetNames);
         Iterator<String> setIterator = nameSet.iterator();
         for (int i = 0; i < quantity; ++i) {
@@ -25,7 +26,9 @@ public class FactoryObjects {
         Set<Sovereign> sovereignSet = new HashSet<>();
         List<String> sovereignNames = Arrays.asList("Leia Organa", "Han Solo", "Luke Skywalker", "Jobal Naberrie", "Pooja Naberrie",
                 "Ruwee Naberrie", "Ryoo Naberrie", "Beru Whitesun Lars ", "Cliegg Lars", "Aika Lars", "Queen Breha Organa", "Darth Vader");
+        Collections.shuffle(sovereignNames,new Random(3));
         Set<String> nameSet = new HashSet<>(sovereignNames);
+
         Iterator<String> setIterator = nameSet.iterator();
         for (int i = 0; i < quantity; ++i) {
             sovereignSet.add(new Sovereign(setIterator.next(),
